391b5218aa7ea0141f77999395a23e255781c7b1
Ozmar Mendoza <ozmarmen@gmail.com>
2022-10-10 23:34:05 -0500

    Clean up countries list

--- src/useViewportSize.js
+++ src/useViewportSize.js
@@ -1,23 +1,22 @@
 import { useEffect, useState } from "react";
 
-export default function useIsMobile(mediaQueryString) {
+export default function useViewportSize(viewportQuery) {
 	const [matchesMedia, setMatchesMedia] = useState(
-		window.matchMedia(mediaQueryString).matches
+		window.matchMedia(viewportQuery).matches
 	);
 
 	useEffect(() => {
-		const mediaWatcher = window.matchMedia(mediaQueryString);
-
 		function updateMatchesMedia(e) {
 			setMatchesMedia(e.matches);
 		}
 
+		const mediaWatcher = window.matchMedia(viewportQuery);
 		mediaWatcher.addEventListener("change", updateMatchesMedia);
 
 		return () => {
 			mediaWatcher.removeEventListener("change", updateMatchesMedia);
 		};
-	});
+	}, [viewportQuery]);
 
 	return matchesMedia;
 }
